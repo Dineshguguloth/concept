69d53efb794a1a5179fb5487f8f64d84da27bb19
Dineshguguloth <gugulothdinesh2003@gmail.com>
2025-03-18 11:53:52 +0530

    'changed'

--- e8.java
+++ e8.java
@@ -1,18 +1,18 @@
-class demo
-{
-    public static void main(String args[])
-    {
-        String sc=null;
-        System.out.println("welcome to java");
-        System.out.println("welcome to java ");
-        try
-        {
-            System.out.println(sc.length());
-        }
-        catch(Exception e)
-        {
-          System.out.println("Enter a String ");
-        }
-         System.out.println("welcome to java 3");
-    }
-} 
+// class demo
+// {
+//     public static void main(String args[])
+//     {
+//         String sc=null;
+//         System.out.println("welcome to java");
+//         System.out.println("welcome to java ");
+//         try
+//         {
+//             System.out.println(sc.length());
+//         }
+//         catch(Exception e)
+//         {
+//           System.out.println("Enter a String ");
+//         }
+//          System.out.println("welcome to java 3");
+//     }
+// } 
